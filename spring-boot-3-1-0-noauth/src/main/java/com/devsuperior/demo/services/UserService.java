f9dea09991964ababa6d2912e51e4d2ca218ce89
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-04-25 15:06:21 -0300

    Work in Process

--- spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
@@ -1,5 +1,9 @@
 package com.devsuperior.demo.services;
 
+
+import com.devsuperior.demo.entities.User;
+import com.devsuperior.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        User user = repository.findByEmail(username);
+
+        if(user == null){
+            throw new UsernameNotFoundException("User not Found");
+        }
+
+        return user;
     }
 }
