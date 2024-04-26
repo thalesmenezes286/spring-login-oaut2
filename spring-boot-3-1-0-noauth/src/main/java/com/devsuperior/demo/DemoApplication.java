2f7457016af8bc92ca15421a3631aea4769edb3c
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-04-26 09:54:43 -0300

    Work in Process

--- spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.devsuperior.demo;
 
-import com.devsuperior.demo.config.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
